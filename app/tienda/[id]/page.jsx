5adddc1901f3bc08810f0404a383806c1e945fb4
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-06-09 07:18:43 -0600

    refactor: Update import path for StoreCategories component

--- app/tienda/[id]/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function StoreItem({ params }) {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <div>Item id: {params.id}</div>
-    </main>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js",
   ],
   theme: {
     extend: {
@@ -13,7 +15,20 @@ module.exports = {
         terciario: "#B892FF",
         cuaternario: "#FFD6EB",
       },
+      fontSize: {
+        "7xl": "5rem",
+        "8xl": "6rem",
+        "9xl": "7rem",
+      },
+    },
+
+    screens: {
+      sm: "300px",
+      md: "600px",
+      lg: "1024px",
+      xl: "1200px",
+      "2xl": "1536px",
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
